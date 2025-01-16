a04601805cfcc4c06ce4788dbc035781a271786d
adventure-capitalist <ashleylaurel@outlook.com>
2025-01-16 23:55:05 +0000

    fix: adjusting the resources section

--- src/routes/resources/resources.jsx
+++ src/routes/resources/resources.jsx
@@ -3,12 +3,12 @@ export const Resources = () => {
 
 	return (
                 <>
-                   <menu>
-                      <a href='/application'>Apply for one of our dogs here</a>
-                      <a href='https://www.thedogspointofview.com/'>Romainian rescue dog training and support</a>
-                      <a href='https://www.paws-hope.com/mia-s-dogs/'>Donate food to the shelter</a>
-                      <a href='https://www.youtube.com/watch?v=Z-pWndEWi-Y&t=1s'>Documentary on Romanian Street Dogs</a>
-                   </menu>
+                   <ul style={{'listStyleType': 'none', background: 'white', width: 'min(90vw, 300px)', margin: 'auto', paddingInlineStart: '0px'}}>  
+                     <li><a href='/application'>Apply for one of our dogs here</a></li> 
+                     <li><a href='https://www.thedogspointofview.com/'>Romainian rescue training & support</a></li> 
+                     <li><a href='https://www.paws-hope.com/mia-s-dogs/'>Donate food to the shelter</a></li> 
+                     <li><a href='https://www.youtube.com/watch?v=Z-pWndEWi-Y&t=1s'>Romanian Street Dogs Documentary</a></li> 
+                   </ul>
                 </>
         )
 }
